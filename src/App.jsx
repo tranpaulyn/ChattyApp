ea226f2ea117e154d89e6cdfdd5b01565ce6f7a2
Paulyn <paulyntran@live.ca>
2019-03-11 23:33:41 +0000

    Moduralized the Main message body and added messages to MessageList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Footer } from './ChatBar.jsx';
+import { Main } from './Message.jsx';
 
 function Navbar() {
   return (
@@ -8,31 +9,6 @@ function Navbar() {
     </nav>
   );
 }
-
-function Main() {
-  return (
-    <main className="messages">
-      <div className="message">
-    <span className="message-username">Anonymous1</span>
-    <span className="message-content">I won't be impressed with technology until I can download food.</span>
-  </div>
-  <div className="message system">
-    Anonymous1 changed their name to nomnom.
-  </div>
-    </main>
-  )
-}
-
-// function Footer() {
-//     return (
-//       <footer className="chatbar">
-//     <input className="chatbar-username" placeholder="Your Name (Optional)" />
-//     <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-//   </footer>
-//     )
-//   }
-
-
 
 class App extends Component {
   constructor() {
