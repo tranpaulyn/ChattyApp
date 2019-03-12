9883b57acad6aca47983dcdcc8205cbd45f00935
Paulyn <paulyntran@live.ca>
2019-03-12 23:59:06 +0000

    Passed AddNewMessage function to ChatBar

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,14 +1,24 @@
 import React, {Component} from 'react';
 
 export class ChatBar extends Component {
-    constructor (){
+    constructor () {
         super();
+        this.keyHandler = this.keyHandler.bind(this);
+    }
+    keyHandler(evt) {
+        if (evt.keyCode === 13) {
+            // console.log(evt.target.value);
+            if (evt.target.value.length > 1) {
+                this.props.addNewMessage(evt.target.value);
+                evt.target.value = null;
+            }
+        };
     }
     render() {
     return (
         <footer className="chatbar">
-      <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.username}/>
-      <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.username} name="username"/>
+      <input className="chatbar-message" onKeyUp={this.keyHandler} placeholder="Type a message and hit ENTER" name="newMessage"/>
     </footer>
       );
     }
