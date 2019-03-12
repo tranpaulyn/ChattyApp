b939b8571f8d9fd894c0d33f827eda9cc14ab541
Paulyn <paulyntran@live.ca>
2019-03-12 22:09:01 +0000

    Added incoming message simulation

--- src/Message.jsx
+++ src/Message.jsx
@@ -2,9 +2,6 @@ import React, {Component} from 'react';
 
 
 export class Message extends Component {
-    constructor (){
-        super();
-    }
     render() {
         return (
             <main className="messages">
