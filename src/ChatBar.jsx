91e69745684d4d3b4ddc81eed7a44b7b4e406f5c
Paulyn <paulyntran@live.ca>
2019-03-14 00:04:21 +0000

    Users can now change their username

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,23 +4,33 @@ export class ChatBar extends Component {
     constructor () {
         super();
         this.keyHandler = this.keyHandler.bind(this);
+        this.changeUsername = this.changeUsername.bind(this);
     }
     keyHandler(evt) {
         if (evt.keyCode === 13) {
-            // console.log(evt.target.value);
-            if (evt.target.value.length > 1) {
+            // Make sure field is not empty
+            if (evt.target.value.length >= 1) {
                 this.props.addNewMessage(evt.target.value);
                 evt.target.value = null;
             }
         };
     }
+
+    changeUsername(evt) {
+        if (evt.keyCode === 13) {
+            this.props.changeName(evt.target.value);
+        }
+    }
+
     render() {
     return (
         <footer className="chatbar">
-      <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.username} name="username"/>
+      <input className="chatbar-username" onKeyUp={this.changeUsername} placeholder="Your Name (Optional)"  name="username"/>
       <input className="chatbar-message" onKeyUp={this.keyHandler} placeholder="Type a message and hit ENTER" name="newMessage"/>
     </footer>
       );
     }
     
 }
+
+// defaultValue={this.props.username}
